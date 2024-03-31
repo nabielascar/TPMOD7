6c7ccda5c1e879805b8c9505cf9a7fa3e1014136
nabielascar <ascarnabiel@gmail.com>
2024-04-01 05:46:18 +0700

    Menambahkan class KuliahMahasiswa1302220080

--- TPMOD7/Program.cs
+++ TPMOD7/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace TPMOD7
+﻿using System.Text.Json;
+
+namespace TPMOD7
 {
-    internal class Program
+    class course
     {
-        static void Main(string[] args)
+        public String kode { get; set; }
+        public String nama { get; set; }
+    }
+
+    internal class KuliahMahasiswa1302220080
+    {
+        class CourseData
         {
-            Console.WriteLine("Hello, World!");
+            public List<Course> courses { get; set; }
+        }
+
+        public static void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("C:\\Docs\\Konstruksi Perangkat Lunak\\TPMOD7\\tp7_1_1302220080 (1)");
+            CourseData dataJson = JsonSerializer.Deserialize<CourseData>(jsonString);
+
+            int i = 1;
+            foreach (var course in dataJson.courses)
+            {
+                Console.WriteLine($"MK {i} {course.kode} - {course.nama}");
+                i++;
+            }
         }
     }
 }
--- TPMOD7/obj/Debug/net8.0/TPMOD7.AssemblyInfo.cs
+++ TPMOD7/obj/Debug/net8.0/TPMOD7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPMOD7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca74a003835801f3c23fbbfd66d779ee5d69319")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPMOD7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPMOD7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
